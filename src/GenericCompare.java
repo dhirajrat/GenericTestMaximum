ff116f90b91b0bbbb811b66c23c9beab426e8091
dhirajrathod <dhirajrathod3000@gmail.com>
2021-07-26 18:21:49 +0530

    Added: added Refactor2 to Generic Class

--- src/GenericCompare.java
+++ src/GenericCompare.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
 
+/*
+    Defining Generic Method
+ */
+class  Generic <T extends Comparable>
+{
+    public T a;
+    public T b;
+    public T c;
+
+    public Generic(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T testMax(){
+        T MAX = GenericCompareOperation.testMax(a,b,c);
+        return MAX;
+    }
+}
+
 class GenericCompareOperation {
     /*
         Method Comparable Max defined
      */
-    public static <T extends Comparable> T compareMax(T a,T b,T c){
+    public static <T extends Comparable> T testMax(T a, T b, T c){
         T mf;
         mf = ( a.compareTo(b) == 1 ? a : b);
         T FMAX;
@@ -22,7 +43,7 @@ public class GenericCompare {
         Integer x= new Integer(sc.nextInt());
         Integer y= new Integer(sc.nextInt());
         Integer z= new Integer(sc.nextInt());
-        Integer IMAX = GenericCompareOperation.compareMax(x,y,z);
+        Integer IMAX = GenericCompareOperation.testMax(x,y,z);
         System.out.println(IMAX+ " is the Maximum Integer number");
 
         //For Float
@@ -30,7 +51,7 @@ public class GenericCompare {
         Float a= new Float(sc.nextFloat());
         Float b= new Float(sc.nextFloat());
         Float c= new Float(sc.nextFloat());
-        Float FMAX = GenericCompareOperation.compareMax(a,b,c);
+        Float FMAX = GenericCompareOperation.testMax(a,b,c);
         System.out.println(FMAX+ " is the Maximum Float number");
 
         //For String
@@ -38,7 +59,7 @@ public class GenericCompare {
         String p = new String("Apple");
         String q = new String("Banana");
         String r = new String("Peach");
-        String SMAX = GenericCompareOperation.compareMax(p,q,r);
+        String SMAX = GenericCompareOperation.testMax(p,q,r);
         System.out.println(SMAX+ " is the Maximum String");
 
     }

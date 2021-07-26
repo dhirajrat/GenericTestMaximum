f08c0b3ab9694eaed3ea80ec28e7adee6af3a81d
dhirajrathod <dhirajrathod3000@gmail.com>
2021-07-26 18:01:35 +0530

    Added: added Refactor1

--- src/GenericCompare.java
+++ src/GenericCompare.java
@@ -1,37 +1,45 @@
 import java.util.Scanner;
 
+class GenericCompareOperation {
+    /*
+        Method Comparable Max defined
+     */
+    public static <T extends Comparable> T compareMax(T a,T b,T c){
+        T mf;
+        mf = ( a.compareTo(b) == 1 ? a : b);
+        T FMAX;
+        FMAX = ( mf.compareTo(c) == 1 ? mf : c);
+        return FMAX;
+    }
+}
+
 public class GenericCompare {
     public static void main(String[] args){
+
+        //For Integer
         System.out.println("Enter 3 Integer numbers to compare");
         Scanner sc = new Scanner(System.in);
         Integer x= new Integer(sc.nextInt());
         Integer y= new Integer(sc.nextInt());
         Integer z= new Integer(sc.nextInt());
-
-        Integer m =new Integer ( x.compareTo(y) == 1 ? x : y);
-        Integer IMAX = new Integer ( m.compareTo(z) == 1 ? m : z);
-
+        Integer IMAX = GenericCompareOperation.compareMax(x,y,z);
         System.out.println(IMAX+ " is the Maximum Integer number");
 
+        //For Float
         System.out.println("Enter 3 Float numbers to compare");
         Float a= new Float(sc.nextFloat());
         Float b= new Float(sc.nextFloat());
         Float c= new Float(sc.nextFloat());
-
-        Float mf =new Float ( a.compareTo(b) == 1 ? a : b);
-        Float FMAX = new Float ( mf.compareTo(c) == 1 ? mf : c);
-
+        Float FMAX = GenericCompareOperation.compareMax(a,b,c);
         System.out.println(FMAX+ " is the Maximum Float number");
 
+        //For String
         System.out.println("Checking MAX between Apple, Banana, Peach");
-
         String p = new String("Apple");
         String q = new String("Banana");
         String r = new String("Peach");
-
-        String ms =new String ( p.compareTo(q) == 1 ? p : q);
-        String SMAX = new String ( ms.compareTo(r) == 1 ? ms : r);
-
+        String SMAX = GenericCompareOperation.compareMax(p,q,r);
         System.out.println(SMAX+ " is the Maximum String");
+
     }
 }
